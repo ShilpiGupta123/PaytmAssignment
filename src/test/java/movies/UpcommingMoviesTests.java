9d15770baccbc8391564909eab0bbbf1ec971264
shilpi gupta <gupta.299shilpi@gmail.com>
2020-06-18 14:58:30 +0530

    Write automation script for the following:-
    * Status code
    * Movie release date: should not be before today’s date
    * Movie Poster URL: should only have .jpg format
    * Paytm movie code: is unique
    * No movie code should have more than 1 language format

---
+++ src/test/java/movies/UpcommingMoviesTests.java
@@ -0,0 +1,78 @@
+
+package movies;
+
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.CoreMatchers.isA;
+import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
+
+import io.restassured.response.Response;
+
+public class UpcommingMoviesTests {
+
+	Logger logger = Logger.getLogger(UpcommingMoviesTests.class);
+	final static String ROOT_URI = "https://apiproxy.paytm.com/v2/movies/upcoming";
+
+	/*
+	 * Create tests for the following cases:-
+	 *
+	 * 1. Status code
+	 * 2. Movie release date: should not be before today’s date
+	 * 3. Movie Poster URL: should only have .jpg format
+	 * 4. Paytm movie code: is unique
+	 * 5. No movie code should have more than 1 language format
+	 */
+	@Test
+	public static void testGetUpcomingMovies() {
+		RestAssured.defaultParser = Parser.JSON;
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+		Response response = get(ROOT_URI);
+		
+		response.then().log().all();
+		
+		response.then().statusCode(200);
+		
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-M-d");
+		
+		response.then()
+			.body("upcomingMovieData.moviePosterUrl", hasItems(endsWith(".jpg")))  
+			.body("upcomingMovieData.language", hasItems(isA(String.class)))
+			.body("upcomingMovieData.releaseDate", hasItems(greaterThan(LocalDate.now().format(dateTimeFormatter))));
+			;
+
+		 ArrayList<String> moviePosterUrl =  response.then().extract().path("upcomingMovieData.paytmMovieCode") ;
+		 Set duplicates = findDuplicates(moviePosterUrl);
+		 assertEquals(true, duplicates.isEmpty());
+	}
+	
+	
+	public static Set<String> findDuplicates(List<String> listContainingDuplicates) {
+		 
+		final Set<String> setToReturn = new HashSet<String>();
+		final Set<String> set1 = new HashSet<String>();
+ 
+		for (String yourInt : listContainingDuplicates) {
+			if (!set1.add(yourInt)) {
+				setToReturn.add(yourInt);
+			}
+		}
+		return setToReturn;
+	}
+}
